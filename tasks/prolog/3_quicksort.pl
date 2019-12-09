2e6de9222338518231d531526751a24d570e8907
Evgenii Timoshchuk <zheny.tim@gmail.com>
2019-12-09 14:38:02 +0300

    add lab 3 done

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,19 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+part(_,[],[],[]).
+qsort([],[]).
+
+part(X, [Head|Tail], [Head|Lowest], Highest):-
+	X > Head,
+	part(X, Tail, Lowest, Highest).
+part(X, [Head|Tail], Lowest, [Head|Highest]):-
+	part(X, Tail, Lowest, Highest).
+
+qsort(L,K):-
+	L = [Head|Tail],
+	part(Head, Tail, List1, List2),
+	qsort(List1, Sorted1),
+	qsort(List2, Sorted2),
+	append(Sorted1, [Head|Sorted2], K).
+
+example:- List = [-7, -8, 1, 7, 0, 5], qsort(List, Sorted), write(Sorted).	
