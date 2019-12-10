6b4924f8c655213c945beb0801d4dd6af7fa0be4
qzsss <56436411+qzsss@users.noreply.github.com>
2019-12-10 22:11:47 +0300

    task 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,4 @@
-package recfun
-import common._
+import scala.collection.immutable._ 
 
 object Main {
   def main(args: Array[String]) {
@@ -9,22 +8,45 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+    println(balance(List('(')))
+    println(balance(List('(', ')')))
+    println(countChange(10, List(5,2,1)))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || r == c) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
-  def balance(chars: List[Char]): Boolean = {
-   
+   def pascal(c: Int, r: Int): Int = {
+    if (r < 0 || c < 0 || c > r) 0
+    else if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
+  /**
+   * Exercise 2 Parentheses Balancing
+   */
+def balance(chars: List[Char]): Boolean = {
+    def f(chars: List[Char], num: Int): Boolean = {
+      (chars,num) match {
+        case (Nil,num) => return num == 0
+        case (c :: tail,num) =>
+          if(num < 0) return false
+          if(c == '(') return f(tail,num+1)
+          if(c == ')') return f(tail,num-1)
+          else return f(tail,num)
+      }
+    }
+    f(chars,0)
+  }
+  
   /**
    * Exercise 3 Counting Change
    * Write a recursive function that counts how many different ways you can make
@@ -33,6 +55,8 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) 1
+    else if (money < 0) 0
+    else if (coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
-}
