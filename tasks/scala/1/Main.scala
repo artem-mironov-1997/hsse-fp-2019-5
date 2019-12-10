54597401c4f6f5a7024527e60d5dd23ec43efe1a
Alina Pereskokova <Alina.Pereskokova@emc.com>
2019-12-10 11:07:00 +0300

    add lab 1 scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,4 @@
-package recfun
-import common._
+import java.security.InvalidParameterException
 
 object Main {
   def main(args: Array[String]) {
@@ -12,27 +11,42 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
+    * Exercise 1
+    */
   def pascal(c: Int, r: Int): Int = {
+    if (r < 0 || c < 0 || c > r) throw new InvalidParameterException
 
+    if (r == 0 || c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
-   * Exercise 2 Parentheses Balancing
-   */
+    * Exercise 2
+    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def count(close: Boolean, acc: Int, chars: List[Char]): Boolean = chars match {
+      case Nil => close && acc == 0
+      case head :: tail if head == '(' => count(false, acc + 1, tail)
+      case head :: tail if head == ')' => count(true, acc - 1, tail)
+      case _ :: tail => count(close, acc, tail)
+    }
+
+    count(true, 0, chars)
   }
 
   /**
-   * Exercise 3 Counting Change
-   * Write a recursive function that counts how many different ways you can make
-   * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
-   * 2 and 3: 2+3.
-   */
+    * Exercise 3
+    */
   def countChange(money: Int, coins: List[Int]): Int = {
 
+    def accumulate(acc: Int, money: Int, coins: List[Int]): Int = coins match {
+      case Nil => acc
+      case _ if money < 0 => acc
+      case _ if money == 0 => acc + 1
+      case _ => accumulate(0, money, coins.tail) + accumulate(0, money - coins.head, coins)
+    }
+
+    if (money == 0) 0
+    else accumulate(0, money, coins.sorted)
   }
 }
