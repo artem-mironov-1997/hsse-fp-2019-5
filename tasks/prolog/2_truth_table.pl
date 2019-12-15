459b29ee44d270d4a76600ee2c7ff334c07095d6
Valerii Poliakov <mr.v.poliakov@gmail.com>
2019-12-15 22:45:49 +0300

    Prolog: Lab 2

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -4,9 +4,24 @@
 	% xor(A, B)
 	% not(A)
 	% equ(A,B)
+
+boolean(A):-A=true;A=false.
+
+and(A,B):-boolean(A),boolean(B),A,B.
+or(A,B):-boolean(A),boolean(B),A;B.
+xor(A,B):-boolean(A),boolean(B),A\=B.
+not(A):-A=false.
+equ(A,B):-boolean(A),boolean(B),A=B.
+
+exec(A,true) :- A, !.
+exec(_,false).
+
+
 % ипользовать предикат truth_table(A,B, expression) для построения таблиц истинности, например:
 % truth_table(A,B,and(A,or(A,B))).
 % true true true
 % true fail true
 % fail true fail
 % fail fail fail
+
+truth_table(A,B,C) :- boolean(A), boolean(B), write(A), write('  '), write(B), write('  '), exec(C, R), writeln(R), false.
