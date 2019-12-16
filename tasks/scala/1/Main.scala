565b0cd448114549f3aec440e303e499269949b2
nionoku <nionoku@gmail.com>
2019-12-16 19:44:16 +0300

    skala: task 1: complete exrcise 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,7 +1,4 @@
-package recfun
-import common._
-
-object Main {
+object Rextester {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
@@ -15,15 +12,18 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
-  def balance(chars: List[Char]): Boolean = {
-   
-  }
+  /*def balance(chars: List[Char]): Boolean = {
+
+  }*/
 
   /**
    * Exercise 3 Counting Change
@@ -32,7 +32,7 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
+  /*def countChange(money: Int, coins: List[Int]): Int = {
 
-  }
+  }*/
 }
