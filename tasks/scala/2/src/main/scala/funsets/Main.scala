93acb6b825afb638f4519af5c2cdf9769fbd37ec
Andrey Koltsov <a99920641@gmail.com>
2019-12-18 17:12:10 +0300

    Try to implement intersect.

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -1,8 +1,11 @@
 package funsets
+import funsets.FunSets.toString
 
 object Main extends App {
   import FunSets._
   val lhsEntrySet = singletonSet(1)
   val rhsEntrySet = singletonSet(2)
-  val testTest = union(lhsEntrySet, rhsEntrySet)
+  val createdUnion = union(lhsEntrySet, rhsEntrySet)
+  println(s"createdUnion: ${FunSets.toString(createdUnion)}")
+
 }
