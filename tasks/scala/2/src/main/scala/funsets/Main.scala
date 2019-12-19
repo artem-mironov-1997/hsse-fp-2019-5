443acdcbc206abbb06a7542a5adc9d6a68d641c5
Evgenii Timoshchuk <zheny.tim@gmail.com>
2019-12-19 14:23:33 +0300

    add scala lab 2

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -2,5 +2,15 @@ package funsets
 
 object Main extends App {
   import FunSets._
-  println(contains(singletonSet(1), 1))
+  //println(contains(singletonSet(1), 1))
+  println("helloworld")
+  var s1: Set = union(singletonSet(1), singletonSet(2))
+  var s2: Set = union(singletonSet(3), singletonSet(2))
+  val fun: Int => Int = _ + 5
+  printSet(s1) //{1,2}
+  printSet(s2) // {2,3}
+  printSet(intersect(s1,s2)) //{2}
+  printSet(diff(s1,s2)) //{1}
+  printSet(map(s1, fun)) //{6,7}
+
 }
