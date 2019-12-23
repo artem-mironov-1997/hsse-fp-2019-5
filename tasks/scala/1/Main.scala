0e87848ba15f05f2b15d16e21a2f4446e87f0b45
li18s <56308532+li18s@users.noreply.github.com>
2019-12-23 22:32:00 +0300

    Update Main.scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,23 +1,25 @@
-package recfun
-import common._
+// package recfun
+//import common._
+import scala.collection.immutable._  
 
 object Main {
   def main(args: Array[String]) {
-    println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
-    }
+    // println("Pascal's Triangle")
+    // for (row <- 0 to 10) {
+    //   for (col <- 0 to row)
+    //     print(pascal(col, row) + " ")
+    //   println()
+    // }
+    //println(balance(List('(', ')', '(', 'x', ')')))
+    //println(countChange(5, List(3,2)))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-     if (c == 0 || r == 0) 1
+    if (c == 0 || r == 0) 1
     else pascal(c - 1, r - 1) + pascal(c - 1, r)
-
   }
 
   /**
@@ -25,13 +27,14 @@ object Main {
    */
   def balance(chars: List[Char]): Boolean = {
     def r(acc: Int, chars: List[Char]): Int = chars match {
-      case '('::xs => r(acc+1, xs)
-      case ')'::xs => r(acc-1, xs)
-      case x::xs => r(acc, xs)
+      case '('::xs   => r(acc+1, xs)
+      case ')'::xs   => r(acc-1, xs)
+      case x::xs     => r(acc, xs)
       case List('(') => acc+1
       case List(')') => acc-1
       case List()    => acc
-   
+    }
+    return r(0, chars) == 0
   }
 
   /**
@@ -42,15 +45,13 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-     def r(last: Int, current: Int): Int = {
-        current match {
+    def r(last: Int, current: Int): Int = {
+      current match {
         case 0 => 1
         case x if x < 0 => 0
         case _ => coins.filter(_ >= last).fold(0){(acc, i) => acc + r(i, current-i)}
       }
     }
     return r(-1, money)
-  }
-
   }
 }
