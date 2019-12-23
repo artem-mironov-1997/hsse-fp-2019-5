01b2d785250d405ed9d10c62ef6cb049af7ae078
Nikita Barsukov <nik0xff@gmail.com>
2019-12-24 00:14:31 +0300

    truth table done

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,24 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+
+boolean(true).
+boolean(false).
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+xor(A, B) :- A \= B.
+not(A) :- or(A,B), not(and(A,B)).
+equ(A, B) :- A = B.
+
+exec(X, true) :- X, !.
+exec(Y, false).
+
+truth_table(A, B, Expression) :- boolean(A), boolean(B), write(A), write('  '), write(B), write('  '), exec(Expression, R), writeln(R), false.
+
+% truth_table(A,B,and(A,or(A,B))).
+% true true true
+% true fail true
+% fail true fail
+% fail fail fail
