d4802ab7526aa3e6777e4a47f904d6df4bd3314a
Dmitrii Leliuhin <dleliuhin@gmail.com>
2019-12-23 15:46:13 +0300

    Feature. Implement scala Lab 2 .

--- tasks/scala/2/src/main/scala/common/package.scala
+++ tasks/scala/2/src/main/scala/common/package.scala
@@ -1,17 +1,20 @@
+//=======================================================================================
+
 import java.io.File
 
-package object common {
+//=======================================================================================
 
+package object common 
+{
   /** An alias for the `Nothing` type.
    *  Denotes that the type should be filled in.
    */
-  type ??? = Nothing
+  	type ??? = Nothing
 
   /** An alias for the `Any` type.
    *  Denotes that the type should be filled in.
    */
-  type *** = Any
-
+  	type *** = Any
   
   /**
    * Get a child of a file. For example,
@@ -20,21 +23,28 @@ package object common {
    * 
    * corresponds to ~/b/c
    */
-  def subFile(file: File, children: String*) = {
-    children.foldLeft(file)((file, child) => new File(file, child))
-  }
+  	def subFile( file: File, children: String* ) = 
+	{
+    	children.foldLeft( file )( ( file, child ) => new File( file, child ) )
+  	}
 
   /**
    * Get a resource from the `src/main/resources` directory. Eclipse does not copy
    * resources to the output directory, then the class loader cannot find them.
    */
-  def resourceAsStreamFromSrc(resourcePath: List[String]): Option[java.io.InputStream] = {
-    val classesDir = new File(getClass.getResource(".").toURI)
-    val projectDir = classesDir.getParentFile.getParentFile.getParentFile.getParentFile
-    val resourceFile = subFile(projectDir, ("src" :: "main" :: "resources" :: resourcePath): _*)
-    if (resourceFile.exists)
-      Some(new java.io.FileInputStream(resourceFile))
-    else
-      None
-  }
+  	def resourceAsStreamFromSrc( resourcePath: List[String] ): Option[java.io.InputStream] = 
+	{
+    	val classesDir = new File( getClass.getResource(".").toURI )
+    	val projectDir = classesDir.getParentFile.getParentFile.getParentFile.getParentFile
+    	val resourceFile = subFile( projectDir, 
+									( "src" :: "main" :: "resources" :: resourcePath ): 
+									_* )
+    	
+		if ( resourceFile.exists )
+      		Some( new java.io.FileInputStream( resourceFile ) )
+    	else
+      		None
+  	}
 }
+
+//=======================================================================================
