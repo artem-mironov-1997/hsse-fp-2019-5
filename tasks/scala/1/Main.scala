47c10aa406d47e537ae491c6c4ba8c82dac4763b
Yulianna Lenkova <45401368+Sloulama@users.noreply.github.com>
2019-12-23 03:46:27 +0300

    Update Main.scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,6 +1,5 @@
 package recfun
-import common._
-
+//import common._
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -9,12 +8,16 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+    println(balance(List('(', ')', '(', 'x', ')')))
+    println(countChange(10, List(3,2)))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
 
   }
 
@@ -22,7 +25,17 @@ object Main {
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def it(chars: List[Char], count: Int): Boolean = {
+      if (chars.isEmpty)
+        count == 0
+      else if (chars.head == '(')
+        it(chars.tail, count + 1)
+      else if (chars.head == ')' && count > 0)
+        it(chars.tail, count - 1)
+      else
+         it(chars.tail, count)
+    }
+    it(chars,0)
   }
 
   /**
@@ -34,5 +47,8 @@ object Main {
    */
   def countChange(money: Int, coins: List[Int]): Int = {
 
+    if (coins.isEmpty || (money - coins.head < 0)) 0
+    else if (money - coins.head == 0) 1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
