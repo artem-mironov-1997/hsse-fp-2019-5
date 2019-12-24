d79808964b67fa73aef56180bb980086d9321dba
NatalijaZ <nzhilkevich@yandex.ru>
2019-12-25 00:12:36 +0400

    Labs

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,7 +1,4 @@
-package recfun
-import common._
-
-object Main {
+object Rextester {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
@@ -9,20 +6,43 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    val listChars = List('(', ')', '(', ')', '(', ')', ')')
+    println("Balance parentheses: " + listChars)
+    println(balance(listChars))
+
+    val money = 7
+    val coins = List(1, 2)
+    println("Change: cash - " + money + ", coins - " + coins)
+    println(countChange(money, coins))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    paranthesesBalanceIsHarmony(chars)
+  }
+
+  private def paranthesesBalanceIsHarmony(chars: List[Char], paranthesesCount: Int = 0): Boolean = {
+    (chars, paranthesesCount) match {
+      case (Nil, num) => num == 0
+      case (ch :: tail, paranthesesCount) =>
+        if (paranthesesCount < 0) false
+        if (ch.toInt == 40) paranthesesBalanceIsHarmony(tail, paranthesesCount + 1)
+        if (ch.toInt == 41) paranthesesBalanceIsHarmony(tail, paranthesesCount - 1)
+        else paranthesesBalanceIsHarmony(tail, paranthesesCount)
+    }
   }
 
   /**
@@ -33,6 +53,11 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (coins.isEmpty || money < 0)
+      0
+    else if (money == 0)
+      1
+    else
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
