70715bd89599c9acbc102afa8fa9c8ff65666d64
SergeiAstafeyv <serg.univer.pl@gmail.com>
2019-12-25 01:07:23 +0300

    Update 3_quicksort.pl

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -3,6 +3,8 @@ q_sort([],Acc,Acc).
 q_sort([H|T],Acc,Sorted):-
 	change(H,T,L1,L2),
 	q_sort(L1,Acc,Sorted1),q_sort(L2,[H|Sorted1],Sorted).
+
+
 change(_,[],[],[]).
 change(H,[X|T],[X|L],G) :- X=<H,change(H,T,L,G).
 change(H,[X|T],L,[X|G]) :- X>H,change(H,T,L,G).
