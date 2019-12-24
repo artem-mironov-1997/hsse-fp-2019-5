2cfec4a28370c0f3282edbc48ea06a5764406780
SergeiAstafeyv <serg.univer.pl@gmail.com>
2019-12-25 01:07:49 +0300

    Update 5_merge.pl

--- tasks/prolog/5_merge.pl
+++ tasks/prolog/5_merge.pl
@@ -1,5 +1,10 @@
 mrg([],[],[]).
+
 mrg([], Y, Y).
+
 mrg(X, [], X).
+
 mrg([H1|List1], [H2|List2], [H1|List]) :- H1 =< H2,!,mrg(List1,[H2|List2],List).
+
 mrg([H1|List1], [H2|List2], [H2|List]) :- mrg([H1|List1],List2,List).
+
