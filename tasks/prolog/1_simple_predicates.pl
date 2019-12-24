c95adf958fb178327d59dcf96aac6e54773a717b
Pavel Vinogradov <vin.pavel13@gmail.com>
2019-12-25 02:15:16 +0300

    Prolog. Lab1. Simple Predicates

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -1,17 +1,48 @@
-% есть набор фактов вида father(person1, person2) (person1 is the father of person2)
-% Необходимо определить набор предикатов:
-% 1. brother(X,Y)    -  определяющий являются ли аргументы братьями
-% 2. cousin(X,Y)     -  определяющий являются ли аргументы двоюродными братьями
-% 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
-% 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
-% 5. используя в качестве исходных данных следующий граф отношений
-	father(a,b).  % 1                 
+	father(a,b).  % 1
 	father(a,c).  % 2
 	father(b,d).  % 3
 	father(b,e).  % 4
 	father(c,f).  % 5
-% указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+
+	brother(X,Y) :- father(Z,Y), father(Z,X), not(X=Y).
+	cousin(X,Y) :- father(Z,Y), father(A,X), brother(A,Z), not(X=Y).
+	grandson(X,Y) :- father(Z,Y), father(X,Z).
+	descendent(X,Y) :- father(X, Y); father(Y, A), descendent(X, A).
+
+	:- writeln('All brothers:').
+	:- forall(brother(X,Y), (write(X), write(' '), writeln(Y))).
+	:- writeln('All cousins:').
+	:- forall(cousin(X,Y), (write(X), write(' '), writeln(Y))).
+	:- writeln('All grandsons:').
+	:- forall(grandson(X,Y), (write(X), write(' '), writeln(Y))).
+	:- writeln('All descendents:').
+	:- forall(descendent(X,Y), (write(X), write(' '), writeln(Y))).
+
+%All brothers:
+%c b
+%b c
+%e d
+%d e
+%All cousins:
+%f d
+%f e
+%d f
+%e f
+%All grandsons:
+%a d
+%a e
+%a f
+%All descendents:
+%a b
+%a c
+%b d
+%b e
+%c f
+%a a
+%b a
+%b a
+%a a
+%c a
+%b b
+%b b
+%c c
